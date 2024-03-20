5fe375d7496b8801e01fd15e9689c38c3f4171a1
emilyganpeijie <ganpeijie3@gmail.com>
2024-03-20 16:47:14 +0800

    update to workable task1.[y

--- task1.py
+++ task1.py
@@ -7,6 +7,13 @@
 inputSTR = input()
 inputLIST = list(inputSTR)
 
+sub = extractSubject(inputLIST)
+print("sub is", sub)
+obj = extractObject(inputLIST)
+print("obj is", obj)
+verb = extractVerb(inputLIST)
+print("verb is", verb)
+
 def caseparse(inputLIST):
     li = [0, 0, 0]
     for i in range(len(inputLIST)):
@@ -17,27 +24,31 @@ def caseparse(inputLIST):
         if inputLIST[i] == 'ま':
             li[2] = i
     return li
-        
     
 def extractSubject(inputLIST):
-    li = caseparse(inputLIST)
+    li0 = inputLIST
+    li = caseparse(li0)
+    li2 = list()
     for i in range(li[0]):
-        return inputLIST[i]
-    
+        li2.append(inputLIST[i])
+    return''.join(li2)
+
 def extractObject(inputLIST):
-    li = caseparse(inputLIST)
+    li0 = inputLIST
+    li = caseparse(li0)
+    li2 = list()
     i = li[0] + 1
-    for i in range(li[1]):
-        return inputLIST[i]
-    
-def extractVerb (inputLIST):
-    li = caseparse(inputLIST)
+    while i < li[1]:
+        li2.append(inputLIST[i])
+        i+=1
+    return''.join(li2)
+
+def extractVerb(inputLIST):
+    li0 = inputLIST
+    li = caseparse(li0)
+    li2 = list()
     i = li[1] + 1
-    for i in range(li[2]):
-        return inputLIST[i]
-
-
-Subject=extractSubject(inputLIST)
-Object=extractObject(inputLIST)
-Verb=extractVerb (inputLIST)
-
+    while i < li[2]:
+        li2.append(inputLIST[i])
+        i+=1
+    return''.join(li2)
