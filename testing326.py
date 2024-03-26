063020332f552153e6be66c92fe6851bc3ae883a
YOSE <143077820+yos564@users.noreply.github.com>
2024-03-27 00:40:45 +0800

    Update testing326.py

--- testing326.py
+++ testing326.py
@@ -2,12 +2,16 @@
 # coding: utf-8
 
 
+#testing1
 import re     #Regualr expression
 role = re.compile(r'が')
 result_split = role.split('私がリンゴを食べます')
 print(result_split)   #['私', 'リンゴを食べます']
 
 
-
-
+#tesing2
+import re
+text = '私がリンゴを食べます'
+result_split = re.split(r'が|を|ます', text)    # pattern
+print(result_split)
 
