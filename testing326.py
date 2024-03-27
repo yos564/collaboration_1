cd095d2669ff29f44ffe4457b141aabf47badd7b
YOSE <143077820+yos564@users.noreply.github.com>
2024-03-27 11:47:41 +0800

    Update testing326.py

--- testing326.py
+++ testing326.py
@@ -20,3 +20,17 @@ for i in result_split:
     if i:
         final.append(i)
 print(final)
+
+#tesing3
+S = '私がリンゴを食べます'
+rule = r'^(.*?)が(.*?)を(.*?)ます$'     #^是符合的那個字串的起點，$是符合那個字串的最後面
+r = re.match (rule, S)                 #re.match來找匹配的字串，
+
+if r:
+    Sub = r.group(1)                   #然後用group的語法來分組
+    Obj = r.group(2)
+    Vb = r.group(3)
+    
+    print("Sub", Sub)
+    print("Obj:", Obj)
+    print('Vb:', Vb)
